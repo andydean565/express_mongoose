2e113cac9e269f0d3f37d064aa40dce2217461d8
andydean565 <andrewdean565@gmail.com>
2018-03-22 09:39:32 +0000

    more lists

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -19,7 +19,7 @@ exports.index = function(req, res) {
 
 // Display list of all books.
 exports.book_list = function(req, res) {
-  Book.find({}, 'title author').populate('author').exec(function (err, list_books) {
+  Book.find({}).populate('author genre').exec(function (err, list_books) {
     if (err) { return next(err); }
     //Successful, so render
     res.render('book_list', { title: 'Book List', book_list: list_books });
